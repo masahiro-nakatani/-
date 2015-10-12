27f09fb1c59eb5104ce5e9e71abc47a0930797bd
makoto <c1ow11er75@gmail.com>
2015-10-12 09:55:27 +0900

    テストコード追加

--- gnupack4etrbcn/home/RobotRun/TestProject/PositionEstimationTest.cpp
+++ gnupack4etrbcn/home/RobotRun/TestProject/PositionEstimationTest.cpp
@@ -56,7 +56,25 @@ namespace
 			getPositionEstimation(pre,1,0,180,1000,pos);
 		}
 
-		ASSERT_EQ(1000,static_cast<int>(pos.X*1000));
+		ASSERT_EQ(0,static_cast<int>(pos.X*1000));
 		ASSERT_EQ(0,static_cast<int>(pos.Y*1000));
+	}
+
+	TEST(position_estimation_4, position_estimation_test)
+	{
+		Position pos;
+		pos.Ang = 0;
+		pos.X=0;
+		pos.Y=0;
+
+
+		for(auto i =0; i < 200; ++i)
+		{
+			Position pre = pos;
+			getPositionEstimation(pre,1,1,180.0/M_PI,1000,pos);
+		}
+
+		ASSERT_EQ(0,static_cast<int>(pos.X));
+		ASSERT_EQ(200,static_cast<int>(pos.Y));
 	}
 }
